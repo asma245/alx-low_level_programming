4a87e6b04b7f6210aa72f0ef182c85734deb4cea
asma245 <asma.hassan.muhamed@gmail.com>
2023-03-24 15:21:44 -0700

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 	void print_alphabet(void)
 	{
